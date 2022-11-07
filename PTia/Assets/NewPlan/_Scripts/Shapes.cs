162eb4ddd3d7e59abe1a4f545d7ef05d52cd9699
Sebastian K Øie <sebagnus@gmail.com>
2022-11-07 21:42:42 +0100

    Started Shapes
    Shapes Class has been created.

---
+++ PTia/Assets/NewPlan/_Scripts/Shapes.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shapes : MonoBehaviour
+{
+    public string name { get; private set; }
+    public Color colour { get; private set; }
+}
