f9200b03c52f2d1f158615dade8c82cc65681a9b
Sebastian K Øie <sebagnus@gmail.com>
2022-11-07 22:47:20 +0100

    Added shapes
    Circle, Square & Triangle

--- PTia/Assets/NewPlan/_Scripts/Shapes.cs
+++ PTia/Assets/NewPlan/_Scripts/Shapes.cs
@@ -7,4 +7,10 @@ public class Shapes : MonoBehaviour
 {
     public string name { get; private set; }
     public Color colour { get; private set; }
+
+    public Text textDisplay;
+    public virtual void DisplayText()
+    {
+
+    }
 }
